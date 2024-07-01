b98353aa8a2fb0052684be4695d9028d35c87136
ccalobeto <calobeto.leon@gmail.com>
2024-07-01 11:31:46 -0500

    scale and responsive

--- src/App.svelte
+++ src/App.svelte
@@ -1,23 +1,32 @@
 <script>
+	import { scaleLinear } from 'd3';
 	const data = [
-		{ x: 155, y: 384, r: 20, fill: '#0000FF' },
-		{ x: 340, y: 238, r: 52, fill: '#FF0AAE' },
-		{ x: 531, y: 151, r: 20, fill: '#00E1FF' },
-		{ x: 482, y: 307, r: 147, fill: '#7300FF' },
-		{ x: 781, y: 91, r: 61, fill: '#0FFB33' },
-		{ x: 668, y: 229, r: 64, fill: '#D400FF' },
+		{ a: 155, b: 384, r: 20, fill: '#0000FF' },
+		{ a: 340, b: 238, r: 52, fill: '#FF0AAE' },
+		{ a: 531, b: 151, r: 20, fill: '#00E1FF' },
+		{ a: 482, b: 307, r: 147, fill: '#7300FF' },
+		{ a: 781, b: 91, r: 61, fill: '#0FFB33' },
+		{ a: 668, b: 229, r: 64, fill: '#D400FF' },
 	];
+
+	let width = 1000;
+	let height = 500;
+	$: xScale = scaleLinear().domain([0, 1000]).range([0, width]);
+	$: yScale = scaleLinear().domain([0, 500]).range([height, 0]);
 </script>
 
-<main>
+<main
+	bind:clientWidth={width}
+	bind:clientHeight={height}
+>
 	<svg
-		width="1000"
-		height="500"
+		width={width}
+		height={height}
 	>
-		{#each data as { x, y, r, fill }}
+		{#each data as { a, b, r, fill }}
 			<circle
-				cx={x}
-				cy={y}
+				cx={xScale(a)}
+				cy={yScale(b)}
 				r={r}
 				fill={fill}
 			/>
@@ -26,6 +35,10 @@
 </main>
 
 <style>
+	main {
+		width: 100vw;
+		height: 100vh;
+	}
 	svg {
 		background: #f3fff0;
 	}
